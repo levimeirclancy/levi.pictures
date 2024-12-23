63a4d3e423db0e0bad24855df00aaf4a15ad4089
Levi Meir Clancy <32032952+levimeirclancy@users.noreply.github.com>
2024-12-23 03:02:20 -0800

    Update index.php

--- index.php
+++ index.php
@@ -4,23 +4,24 @@ echo "<br><br>";
 
 echo "<br><br>";
 
-echo "<a href='https://mailchi.mp/8c1c7dedc1fe/levi-meir-clancy'>mailing list</a>";
+echo "portfolio albums";
+echo "<br>";
+echo "— <a href='https://adobe.ly/3G3u7Rf'>personality portraits</a>";
+echo "<br>";
+echo "— <a href='https://adobe.ly/3xyGMsd'>love and family</a>";
 
 echo "<br><br>";
 
-echo "<a href='https://adobe.ly/3G3u7Rf'>personality portraits</a>";
+echo "other albums";
+echo "— <a href='https://stock.adobe.com/contributor/209506797/levi-meir-clancy/'>Adobe Stock</a>";
+echo "<br>";
+echo "— <a href='https://unsplash.com/@levimeirclancy'>Unsplash</a>";
 
 echo "<br><br>";
 
-echo "<a href='https://adobe.ly/3xyGMsd'>love and family</a>";
-
-echo "<br><br>";
-
-echo "<a href='https://stock.adobe.com/contributor/209506797/levi-meir-clancy/'>Adobe Stock</a>";
-
-echo "<br><br>";
-
-echo "<a href='https://unsplash.com/@levimeirclancy'>Unsplash</a>";
+echo "additional links";
+echo "— <a href='https://mailchi.mp/8c1c7dedc1fe/levi-meir-clancy'>mailing list</a>";
+echo "— <a href='https://levi.news'>levi.news</a>";
 
 echo "<br><br>";
 
