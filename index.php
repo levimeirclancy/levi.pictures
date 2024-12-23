d4bddf25761fdca134a930c48a1cedcfa719cdc7
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2024-12-23 04:13:54 -0800

    Update index.php

--- index.php
+++ index.php
@@ -16,13 +16,13 @@ echo "<br><br>";
 
 echo "Portfolio albums";
 echo "<br>";
-echo "— <a href='https://adobe.ly/3G3u7Rf'>personality portraits</a>";
+echo "— <a href='https://adobe.ly/3G3u7Rf'>Personality portraits</a>";
 echo "<br>";
-echo "— <a href='https://adobe.ly/3xyGMsd'>love and family</a>";
+echo "— <a href='https://adobe.ly/3xyGMsd'>Love and family</a>";
 
 echo "<br><br>";
 
-echo "Other albums";
+echo "Other portfolios";
 echo "<br>";
 echo "— <a href='https://stock.adobe.com/contributor/209506797/levi-meir-clancy/'>Adobe Stock</a>";
 echo "<br>";
@@ -32,7 +32,7 @@ echo "<br><br>";
 
 echo "Additional links";
 echo "<br>";
-echo "— <a href='https://mailchi.mp/8c1c7dedc1fe/levi-meir-clancy'>mailing list</a>";
+echo "— <a href='https://mailchi.mp/8c1c7dedc1fe/levi-meir-clancy'>Mailing list</a>";
 echo "<br>";
 echo "— <a href='https://levi.news'>levi.news</a>";
 
