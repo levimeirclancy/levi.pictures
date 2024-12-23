b00b008db2141c4b2ab817cb6b4e2d5251e575a2
Levi Meir Clancy <32032952+levimeirclancy@users.noreply.github.com>
2024-12-23 02:55:23 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?
+
+echo "<br><br>";
+
+echo "<br><br>";
+
+echo "<a href='https://mailchi.mp/8c1c7dedc1fe/levi-meir-clancy'>mailing list</a>";
+
+echo "<br><br>";
+
+echo "<a href='https://adobe.ly/3G3u7Rf'>personality portraits</a>";
+
+echo "<br><br>";
+
+echo "<a href='https://adobe.ly/3xyGMsd'>love and family</a>";
+
+echo "<br><br>";
+
+echo "<a href='https://stock.adobe.com/contributor/209506797/levi-meir-clancy/'>Adobe Stock</a>
+
+echo "<br><br>";
+
+echo "<a href='https://unsplash.com/@levimeirclancy'>Unsplash</a>";
+
+echo "<br><br>";
+
+echo "<br><br>";
+
+?>
